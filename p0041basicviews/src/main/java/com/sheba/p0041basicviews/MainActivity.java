26681d32f9b4629b7810021eae4461ef3669a531
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2021-02-21 14:21:32 +0300

    Init commit

---
+++ p0041basicviews/src/main/java/com/sheba/p0041basicviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sheba.p0041basicviews;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
